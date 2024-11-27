dfc9806af779f8488806a4c6787762ace56a09a0
manoj0430 <manojl0430@gmail.com>
2024-11-27 23:28:26 +0530

    Config Firebase

---
+++ src/utilis/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBaDsTrmt-pmHEYZv90T12p114c9Saq3vU",
+  authDomain: "netflixgpt-c3010.firebaseapp.com",
+  projectId: "netflixgpt-c3010",
+  storageBucket: "netflixgpt-c3010.firebasestorage.app",
+  messagingSenderId: "1005770139134",
+  appId: "1:1005770139134:web:e48deb06bd6fe3ba64ccaf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
